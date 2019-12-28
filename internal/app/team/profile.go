3e2021d6b25b57ce1271b55f3a8063a36e15668e
axetroy <axetroy.dev@gmail.com>
2019-12-28 17:45:11 +0800

    update

---
+++ internal/app/team/profile.go
@@ -0,0 +1,59 @@
+package team
+
+import (
+	"errors"
+	"github.com/axetroy/terminal/internal/app/db"
+	"github.com/axetroy/terminal/internal/app/exception"
+	"github.com/axetroy/terminal/internal/app/schema"
+	"github.com/axetroy/terminal/internal/library/controller"
+	"github.com/axetroy/terminal/internal/library/helper"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"github.com/mitchellh/mapstructure"
+	"net/http"
+	"time"
+)
+
+func (s *Service) GetMyProfile(c controller.Context, teamID string) (res schema.Response) {
+	var (
+		data schema.TeamMember
+		err  error
+	)
+
+	defer func() {
+		if r := recover(); r != nil {
+			switch t := r.(type) {
+			case string:
+				err = errors.New(t)
+			case error:
+				err = t
+			default:
+				err = exception.Unknown
+			}
+		}
+
+		helper.Response(&res, &data, nil, err)
+	}()
+
+	teamMemberInfo := db.TeamMember{}
+
+	if err = db.Db.Where(db.TeamMember{TeamID: teamID, UserID: c.Uid}).Preload("User").First(&teamMemberInfo).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			err = exception.NoPermission
+		}
+		return
+	}
+
+	if err = mapstructure.Decode(teamMemberInfo.User, &data.ProfilePublic); err != nil {
+		return
+	}
+
+	data.Role = teamMemberInfo.Role
+	data.CreatedAt = teamMemberInfo.CreatedAt.Format(time.RFC3339Nano)
+
+	return
+}
+
+func (s *Service) GetMyProfileRouter(c *gin.Context) {
+	c.JSON(http.StatusOK, s.GetMyProfile(controller.NewContextFromGinContext(c), c.Param("team_id")))
+}
